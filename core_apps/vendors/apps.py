982cdeabb426bcc7ed4448daa6681aa699489cd2
Arshid <arshidbaba93@gmail.com>
2023-12-13 19:28:20 +0530

    Additional settings

--- core_apps/vendors/apps.py
+++ core_apps/vendors/apps.py
@@ -4,5 +4,5 @@ from django.utils.translation import gettext_lazy as _
 
 class VendorsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "core_apps_vendors"
+    name = "core_apps.vendors"
     verbose_name = _("Vendors")
--- fatmug/settings/base.py
+++ fatmug/settings/base.py
@@ -40,7 +40,7 @@ THIRD_PARTY_APPS = [
     "corsheaders",
 ]
 
-LOCAL_APPS = []
+LOCAL_APPS = ["core_apps.vendors", "core_apps.users"]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
